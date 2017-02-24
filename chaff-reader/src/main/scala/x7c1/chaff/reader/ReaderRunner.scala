635ff40d5aefde313daa8d73547fddf0aeb71d6d
x7c1 <jmtaro+x7c1@gmail.com>
2017-02-25 02:07:37 +0900

    remove unused methods

--- chaff-reader/src/main/scala/x7c1/chaff/reader/ReaderRunner.scala
+++ chaff-reader/src/main/scala/x7c1/chaff/reader/ReaderRunner.scala
@@ -4,14 +4,14 @@ import scala.collection.immutable
 
 object ReaderRunner {
 
-  def run3[X, A](x: X, reader: Reader[X, A]): A = {
+  def run[X, A](reader: BaseReader[X, A]): X => A = x => {
 
     @scala.annotation.tailrec
     def loop(stack: List[Any]): A = {
       stack match {
-        case Reader2(fa, f) :: tail =>
-          loop(fa :: f :: tail)
-        case (fa: Reader[X, _]) :: tail =>
+        case (head: FlatMapped[_, _, _]) :: tail =>
+          loop(head.fa :: head.f :: tail)
+        case (fa: BaseReader[X, _]) :: tail =>
           loop(fa.run(x) :: tail)
         case head :: f :: tail =>
           loop(f.asInstanceOf[Any => Any](head) :: tail)
@@ -19,47 +19,8 @@ object ReaderRunner {
           head.asInstanceOf[A]
       }
     }
+
     loop(List(reader))
-  }
-
-  def run2[X, A](x: X, reader: Reader[X, A]): A = {
-
-    @scala.annotation.tailrec
-    def loop(stack: immutable.Stack[Any]): A = {
-      stack.pop2 match {
-        case (Reader2(fa, f), next) =>
-          loop(next.push(f, fa))
-        case (fa: Reader[X, _], next) =>
-          loop(next push fa.run(x))
-        case (value, next) if next.nonEmpty =>
-          val (f, next2) = next.pop2
-          loop(next2 push f.asInstanceOf[Any => Any](value))
-        case (value, _) =>
-          value.asInstanceOf[A]
-      }
-    }
-    loop(immutable.Stack(reader))
-  }
-
-  def run[X, A](x: X, reader: Reader[X, A]): A = {
-    val stack = collection.mutable.Stack[Any](reader)
-    var a: Option[A] = None
-    while (stack.nonEmpty) {
-      val current = stack.pop()
-      current match {
-        case Reader2(fa, f) =>
-          stack push f
-          stack push fa
-        case fa: Reader[X, _] =>
-          stack push fa.run(x)
-        case value if stack.nonEmpty =>
-          val f = stack.pop().asInstanceOf[Any => Any]
-          stack push f(value)
-        case value =>
-          a = Some(value.asInstanceOf[A])
-      }
-    }
-    a.get
   }
 
   def traverse(node: ReaderNodeSample)(f: String => Unit) = {
