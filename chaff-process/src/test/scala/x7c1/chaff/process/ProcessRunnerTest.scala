0a3e7d2c6d8711c80e94387ede5d883f510a8698
x7c1 <jmtaro+x7c1@gmail.com>
2017-03-04 01:56:19 +0900

    add test for ProcessRunner#exitCode

--- chaff-process/src/test/scala/x7c1/chaff/process/ProcessRunnerTest.scala
+++ chaff-process/src/test/scala/x7c1/chaff/process/ProcessRunnerTest.scala
@@ -1,5 +1,7 @@
 package x7c1.chaff.process
 
+import java.io.IOException
+
 import org.scalatest.{FlatSpecLike, Matchers}
 import sbt.ProcessLogger
 
@@ -8,7 +10,7 @@ import scala.util.{Left, Right}
 
 class ProcessRunnerTest extends FlatSpecLike with Matchers {
 
-  "#lines" can "return Seq[String] if succeeded" in {
+  ".lines" can "return Seq[String] if succeeded" in {
     val logger = new BufferedLogger
     val runner = ProcessRunner(Seq("ls"))
 
@@ -22,7 +24,7 @@ class ProcessRunnerTest extends FlatSpecLike with Matchers {
     logger.lines should contain("[run] ls")
   }
 
-  "#lines" should "return ProcessError to non-existent command" in {
+  ".lines" should "return ProcessError to non-existent command" in {
     val logger = new BufferedLogger
     val runner = ProcessRunner(Seq("non-existent-command"))
 
@@ -35,7 +37,7 @@ class ProcessRunnerTest extends FlatSpecLike with Matchers {
     logger.lines should contain("[run] non-existent-command")
   }
 
-  "#lines" should "return ProcessError if not succeeded" in {
+  ".lines" should "return ProcessError if not succeeded" in {
     val logger = new BufferedLogger
     val runner = ProcessRunner(Seq("ls", "foobar"))
 
@@ -48,6 +50,34 @@ class ProcessRunnerTest extends FlatSpecLike with Matchers {
     logger.lines should contain("[run] ls foobar")
   }
 
+  ".exitCode" should "return zero if succeeded" in {
+    val logger = new BufferedLogger
+    val runner = ProcessRunner(Seq("ls"))
+
+    runner.exitCode run logger should be(0)
+
+    logger.lines should contain("[run] ls")
+    logger.lines should contain("LICENSE")
+    logger.lines should contain("README.md")
+  }
+
+  ".exitCode" should "throw IOException to non-existent command" in {
+    val logger = new BufferedLogger
+    val runner = ProcessRunner(Seq("non-existent-command"))
+
+    a[IOException] shouldBe thrownBy {
+      runner.exitCode run logger
+    }
+    logger.lines should contain("[run] non-existent-command")
+  }
+
+  ".exitCode" should "return non-zero if not succeeded" in {
+    val logger = new BufferedLogger
+    val runner = ProcessRunner(Seq("ls", "foobar"))
+
+    runner.exitCode run logger shouldNot be(0)
+    logger.lines should contain("[run] ls foobar")
+  }
 }
 
 class BufferedLogger extends ProcessLogger {
